b0a183eb87b8536493dc2781a189a210a0775968
leticia <marreiro122@gmail.com>
2026-04-23 17:25:30 -0300

    Card Updated

--- ticiaApp/cardNews.swift
+++ ticiaApp/cardNews.swift
@@ -7,29 +7,62 @@
 
 import SwiftUI
 
+struct news {
+    let manchete: String;
+    let texto: String;
+    let fonte: String;
+}
+
+var newsList: [news] = [
+    news(manchete: "URGENTE!! Vacina CAUSA AUTISMO em CRIANÇAS!!", texto: "COMPARTILHE ANTES QUE REMOVAM!! Fontes ciemtificas afirma que vacinas causa AUTISMO!! Médicos NÃO querem que você saiba disso!! COMPARTILHE com AMIGOS aaaaaaaaaaaaaaaaaaaaaaaaaLWIRFHIEHRFIUEIHRFIUHEDISRKFJDKLSRHFUKEDRHFUIOEHRe FAMÍLIA!!", fonte: "Grupo do WhatsApp"),
+    news(manchete: "Notícia 2", texto: "Texto da notícia 2", fonte: "Grupo do WhatsApp"),
+    news(manchete: "Notícia 3", texto: "Texto da notícia 3", fonte: "Grupo do WhatsApp"),
+]
+
 struct cardNews: View {
     var body: some View {
-        VStack (alignment: .leading){
-            Image("cardImage")
-                .resizable()
-                .frame(width: 300, height: 200)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-            cardText.padding(.horizontal, 1)
+        
+        VStack{
+            GroupBox{
+                Image("cardImage")
+                    .resizable()
+                    .frame(width: 295, height: 173)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .shadow(radius: 12)
+                    .padding(0)
+                VStack(alignment: .leading){
+                    Text(newsList[0].manchete)
+                        .font(.headline)
+                    
+                    ScrollView(.vertical, showsIndicators: true) {
+                        Text(newsList[0].texto)
+                            .font(.footnote)
+                    }
+                    .frame(height: 91)
+                    .foregroundStyle(Color(.secondaryLabel))
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .leading){
+                    Text("Fonte: \(newsList[0].fonte)")
+                        .font(.subheadline)
+                        .bold()
+                        .foregroundStyle(Color(.secondaryLabel))
+                }
+                    
+                
+            }
+            .frame(width: 327, height: 411)
+            .backgroundStyle(Color.clear)
+            .padding(0)
         }
-        .clipShape(RoundedRectangle(cornerRadius: 10))
-        .background(Color.gray)
-        .shadow(radius: 12)
-    }
-    
-    var cardText: some View {
-        VStack (alignment: .leading){
-            Text("Notícia")
-                .font(.headline)
-            Text("Texto da notícia")
-                .foregroundStyle(Color(.secondaryLabel))
-        }
+        .background(Color(.pink))
+        .clipShape(RoundedRectangle(cornerRadius: 15))
+        .frame(width: 327, height: 411)
     }
 }
+
 
 #Preview {
     cardNews()
