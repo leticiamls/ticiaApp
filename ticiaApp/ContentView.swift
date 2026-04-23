b4fbe77907ef2cfb36b19ad7aa1994771d564dc6
leticia <marreiro121@gmail.com>
2026-04-23 08:40:42 -0300

    Initial Commit

---
+++ ticiaApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ticiaApp
+//
+//  Created by User on 23/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ticiaApp/ticiaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ticiaAppApp.swift
+//  ticiaApp
+//
+//  Created by User on 23/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ticiaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
