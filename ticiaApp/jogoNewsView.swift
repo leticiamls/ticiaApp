80b2af7c243ac0ab7cf0cafd43a20ed70a270479
marcos-v-costa <marcoscosta.marq@gmail.com>
2026-04-27 10:43:50 -0300

    jogoNewsView v1

--- ticiaApp/jogoNewsView.swift
+++ ticiaApp/jogoNewsView.swift
@@ -8,46 +8,126 @@
 import SwiftUI
 
 struct cardNews: View {
+    @State private var progressGame: Double = 0.80
+
     var body: some View {
+        NavigationStack {
+        HStack{
+            Gauge(value: progressGame){
+                //
+            }
+            .tint(Color(.black))
+            HStack{
+                HStack{
+                    Image(systemName: "hand.thumbsup.fill")
+                    Text("80%")
+                }
+                .foregroundStyle(Color(.green))
+                HStack{
+                    Image(systemName: "exclamationmark.triangle.fill")
+                    Text("20%")
+                }
+                .foregroundStyle(Color(.red))
+            }
+            .frame(maxWidth: 144)
+        }
+            .padding(18)
+            .font(Font.system(size: 16, weight: .bold))
+            .frame(width: 402, height: 40)
         
-        VStack{
-            GroupBox{
-                Image("cardImage")
-                    .resizable()
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .shadow(radius: 12)
-                    .padding(0)
-                VStack(alignment: .leading){
-                    Text(newsList[0].manchete)
-                        .font(Font.custom("Fredoka-SemiBold", size: 24))
-                    ScrollView(.vertical, showsIndicators: true) {
+        Spacer()
+            .frame(height: 10)
+        
+        VStack (spacing: 90){
+            VStack {
+                GroupBox{
+                    Image("cardImage")
+                        .resizable()
+                        .frame(width: 300, height: 150)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                    VStack(alignment: .leading){
+                        Text(newsList[0].manchete)
+                            .font(Font.custom("Fredoka-SemiBold", size: 24))
                         Text(newsList[0].texto)
                             .font(Font.custom("Fredoka-Regular", size: 16))
-                    }
-                    .foregroundStyle(Color(.secondaryLabel))
-                    
-                    Spacer()
-                    
-                    VStack(alignment: .leading){
-                        Text("Fonte: \(newsList[0].fonte)")
-                            .font(.subheadline)
-                            .bold()
-                            .font(Font.custom("Fredoka-Regular", size: 12))
+                            .frame(maxHeight: 100)
+                            .foregroundStyle(Color(.secondaryLabel))
+                        
+                        
+                        VStack(alignment: .leading){
+                            Text("Fonte: \(newsList[0].fonte)")
+                                .font(Font.custom("Fredoka-Regular", size: 15))
+                        }
                     }
                 }
+                .background(RoundedRectangle(cornerRadius: 20)
+                    .fill(Color.white)
+                    .stroke(Color(.lightGray), lineWidth: 1))
+                .foregroundColor(Color("Black"))
+                .frame(maxWidth: 327 , maxHeight: 393)
+                .backgroundStyle(Color.clear)
+            }
+            
+            
+            HStack (spacing: 30){
+                Button {
+                    //botao para publicar a noticia
+                }
+                label: {
+                    HStack{
+                        Image(systemName: "checkmark")
+                        Text("Publicar")
+                    }
+                    .padding(10)
+                    .frame(width: 150)
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(Color(.black))
+                .font(Font.system(size: 24, weight: .bold))
+                
+                Button {
+                    //botao para excluir a noticia
+                }
+                label: {
+                    HStack{
+                        Image(systemName: "xmark")
+                        Text("Excluir")
+                    }
+                    .padding(18)
+                    .frame(width: 150)
+                    .background(RoundedRectangle(cornerRadius: 30).stroke(Color(.black), lineWidth: 2))
+                }
+                .tint(Color(.black))
+                .font(Font.system(size: 24, weight: .bold))
             }
         }
-        .background(Color(.white))
-        .foregroundColor(Color("Black"))
-        .clipShape(RoundedRectangle(cornerRadius: 15))
-        .frame(maxWidth: .infinity , maxHeight: .infinity)
-        .backgroundStyle(Color.clear)
-        .padding(.horizontal, 37)
-        .padding(.top, 167).padding(.bottom, 200)
+            
+                .toolbar{
+                    ToolbarItem(placement: .topBarLeading){
+                        Button(action: {
+                            
+                            //
+                        }, label: {
+                            Image(systemName: "chevron.left")
+                        })
+                    }
+                    
+                    ToolbarItem(placement: .title){
+                        Button(action: {
+                            
+                            //
+                        }, label: {
+                            Text("NOTÍCIA 1 DE 10")
+                                .font(Font.custom("Fredoka-Medium", size: 16))
+                                .kerning(1)
+                        })
+                    }
+                }
+                .frame(height: 720)
+        }
     }
 }
 
 #Preview {
     cardNews()
 }
-
